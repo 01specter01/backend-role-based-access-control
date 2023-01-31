3ad5a651bd5570a8ecbcec9ea42439bb6a6f3c2a
Leo Volwahsen <volwahsenl@gmail.com>
2023-01-31 13:04:42 +0100

    n

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-// import checkToken from "./middlewares/checkToken.js";
+import checkToken from "./middlewares/checkToken.js";
 import authRouter from "./routes/auth.js";
-// import protectedRouter from "./routes/protectedRoutes.js";
+import protectedRouter from "./routes/protectedRoutes.js";
 const app = express();
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`listening on port ${port}`));
 
-// import checkMethodMiddleware from "./middlewares/checkMethod.js";
-// app.use(checkMethodMiddleware);
+import checkMethodMiddleware from "./middlewares/checkMethod.js";
+app.use(checkMethodMiddleware);
 
 app.use(express.json());
 
 app.use("/auth", authRouter);
-// app.use("/protected-routes", checkToken, protectedRouter);
+app.use("/protected-routes", checkToken, protectedRouter);
 
 app.use((error, req, res, next) => {
     console.error(error);
